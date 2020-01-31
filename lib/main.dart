ba5fa8a572b288305a175c10fc3424c67193d553
hasibul-99 <hasibul_uap@outlook.com>
2020-02-01 01:22:02 +0600

    containrs and margin padding

--- lib/main.dart
+++ lib/main.dart
@@ -17,52 +17,58 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[400],
       ),
-      body: Center(
-        //=================        text       ====================
-//          child: Text(
-//            'Hello All!',
-//            style: TextStyle(
-//                fontSize: 20.0,
-//                fontWeight: FontWeight.bold,
-//                letterSpacing: 2.0,
-//                color: Colors.grey[600],
-//                fontFamily: 'IndieFlower'
-//            ),
-//          ),
-       //==================       Images       ==========================
-//          child: Image(
-////            image: NetworkImage('https://picsum.photos/seed/picsum/200/300'),
-//            image: AssetImage('assets/tree-736885__340.webp'),
-//          )
-      //===================       Icon         ===========================
-//        child: Icon(
-//          Icons.all_inclusive,
-//          color: Colors.deepOrange,
-//          size: 70.0,
-//        ),
-      //===================     RaiseButton, FlatButton  ========================
-//        child: FlatButton(
-//            onPressed: () {
-//              print('You clicked me ');
-//            },
-//            child: Text('click me'),
-//            color: Colors.lightBlue,
-//        ),
-      //===================     Button with icon    ===============================
-//        child: RaisedButton.icon(
-//            onPressed: () {},
-//            icon: Icon(
-//              Icons.mail_outline
-//            ),
-//            label: Text('mail me'),
-//            color: Colors.amberAccent
-//        ),
-      // ==================     IconButton      ===============================
-        child: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.offline_bolt),
-          color: Colors.amberAccent
-        ),
+//      body: Center(
+//        //=================        text       ====================
+////          child: Text(
+////            'Hello All!',
+////            style: TextStyle(
+////                fontSize: 20.0,
+////                fontWeight: FontWeight.bold,
+////                letterSpacing: 2.0,
+////                color: Colors.grey[600],
+////                fontFamily: 'IndieFlower'
+////            ),
+////          ),
+//       //==================       Images       ==========================
+////          child: Image(
+//////            image: NetworkImage('https://picsum.photos/seed/picsum/200/300'),
+////            image: AssetImage('assets/tree-736885__340.webp'),
+////          )
+//      //===================       Icon         ===========================
+////        child: Icon(
+////          Icons.all_inclusive,
+////          color: Colors.deepOrange,
+////          size: 70.0,
+////        ),
+//      //===================     RaiseButton, FlatButton  ========================
+////        child: FlatButton(
+////            onPressed: () {
+////              print('You clicked me ');
+////            },
+////            child: Text('click me'),
+////            color: Colors.lightBlue,
+////        ),
+//      //===================     Button with icon    ===============================
+////        child: RaisedButton.icon(
+////            onPressed: () {},
+////            icon: Icon(
+////              Icons.mail_outline
+////            ),
+////            label: Text('mail me'),
+////            color: Colors.amberAccent
+////        ),
+//      // ==================     IconButton      ===============================
+////        child: IconButton(
+////          onPressed: () {},
+////          icon: Icon(Icons.offline_bolt),
+////          color: Colors.amberAccent
+////        ),
+//      ),
+      body: Container(
+        color: Colors.grey[400],
+        child: Text('hello'),
+        padding: EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 25),
+        margin: EdgeInsets.all(30),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
