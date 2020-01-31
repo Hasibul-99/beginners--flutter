e3a24b7f3c4e2a97a01b64c1c8cdb71e7ceb50ca
hasibul-99 <hasibul_uap@outlook.com>
2020-02-01 00:24:34 +0600

    button and icons

--- lib/main.dart
+++ lib/main.dart
@@ -18,16 +18,51 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[400],
       ),
       body: Center(
-          child: Text(
-            'Hello All!',
-            style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-                color: Colors.grey[600],
-                fontFamily: 'IndieFlower'
-            ),
-          )
+        //=================        text       ====================
+//          child: Text(
+//            'Hello All!',
+//            style: TextStyle(
+//                fontSize: 20.0,
+//                fontWeight: FontWeight.bold,
+//                letterSpacing: 2.0,
+//                color: Colors.grey[600],
+//                fontFamily: 'IndieFlower'
+//            ),
+//          ),
+       //==================       Images       ==========================
+//          child: Image(
+////            image: NetworkImage('https://picsum.photos/seed/picsum/200/300'),
+//            image: AssetImage('assets/tree-736885__340.webp'),
+//          )
+      //===================       Icon         ===========================
+//        child: Icon(
+//          Icons.all_inclusive,
+//          color: Colors.deepOrange,
+//          size: 70.0,
+//        ),
+      //===================     RaiseButton, FlatButton  ========================
+//        child: FlatButton(
+//            onPressed: () {
+//              print('You clicked me ');
+//            },
+//            child: Text('click me'),
+//            color: Colors.lightBlue,
+//        ),
+      //===================     Button with icon    ===============================
+//        child: RaisedButton.icon(
+//            onPressed: () {},
+//            icon: Icon(
+//              Icons.mail_outline
+//            ),
+//            label: Text('mail me'),
+//            color: Colors.amberAccent
+//        ),
+      // ==================     IconButton      ===============================
+        child: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.offline_bolt),
+          color: Colors.amberAccent
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
