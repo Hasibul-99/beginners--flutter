2c801457927597fa90639fa5fd3d19c6709e4952
hasibul-99 <hasibul_uap@outlook.com>
2020-01-31 23:07:11 +0600

    style, color and font change process

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,21 @@ void main() => runApp(MaterialApp(
       backgroundColor: Colors.red[400],
     ),
     body: Center(
-      child: Text('Hello')
+      child: Text(
+        'Hello All!',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower'
+        ),
+      )
     ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {},
       child: Text('Click'),
+      backgroundColor: Colors.red[600],
     ),
   ),
 ));
