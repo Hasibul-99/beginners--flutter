1ddb2a57a469ddb288061da6208c3edd8c505125
hasibul-99 <hasibul_uap@outlook.com>
2020-02-01 19:17:02 +0600

    list of data

--- lib/main.dart
+++ lib/main.dart
@@ -1,214 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Ninjacard()
+  home: QuoteList()
 ));
 
-//=======================       State less        ================================
-//class Ninjacard extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      backgroundColor: Colors.grey[900],
-//      appBar: AppBar(
-//        title: Text('Ninja ID Card'),
-//        centerTitle: true,
-//        backgroundColor: Colors.grey[850],
-//        elevation: 0.0,
-//      ),
-//      body: Padding(
-//        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-//        child: Column(
-//          crossAxisAlignment: CrossAxisAlignment.start,
-//          children: <Widget>[
-//            Center(
-//              child: CircleAvatar(
-//                backgroundImage: AssetImage('assets/tree-736885__340.webp'),
-//                radius: 40.0
-//              ),
-//            ),
-//            Divider(
-//              height: 60.0,
-//              color: Colors.grey[800],
-//            ),
-//            Text(
-//              'Name',
-//              style: TextStyle(
-//                color: Colors.grey,
-//                letterSpacing: 2.0,
-//              ),
-//            ),
-//            SizedBox(height: 10.0),
-//            Text(
-//              'Hasibul Hasan',
-//              style: TextStyle(
-//                color: Colors.orange,
-//                letterSpacing: 2.0,
-//                fontSize: 28.0,
-//                fontWeight: FontWeight.bold,
-//              ),
-//            ),
-//            SizedBox(height: 30.0),
-//            Text(
-//              'Occupation',
-//              style: TextStyle(
-//                color: Colors.grey,
-//                letterSpacing: 2.0,
-//              ),
-//            ),
-//            SizedBox(height: 10.0),
-//            Text(
-//              'Sofware Developer',
-//              style: TextStyle(
-//                color: Colors.orange,
-//                letterSpacing: 2.0,
-//                fontSize: 28.0,
-//                fontWeight: FontWeight.bold,
-//              ),
-//            ),
-//            SizedBox(height: 30.0,),
-//            Row(
-//              children: <Widget>[
-//                Icon(
-//                  Icons.email,
-//                  color: Colors.grey[400],
-//                ),
-//                SizedBox(width: 10.0,),
-//                Text(
-//                  'Haisbul_uap@gmail.com',
-//                  style: TextStyle(
-//                    color: Colors.grey[400],
-//                    fontSize: 18.0,
-//                    letterSpacing: 1.0
-//                  ),
-//                )
-//              ],
-//            )
-//          ],
-//        ),
-//      )
-//    );
-//  }
-//}
-
-//===============     State full   =================================
-
-class Ninjacard extends StatefulWidget {
+class QuoteList extends StatefulWidget {
   @override
-  _NinjacardState createState() => _NinjacardState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _NinjacardState extends State<Ninjacard> {
-  int ninjaLevel = 0;
+class _QuoteListState extends State<QuoteList> {
+
+  List<String> quotes = [
+    'You are Amazing buddy. ',
+    'If only school curriculum were like this  and teachers  were like you world could be a better place',
+    'Be yourself; everyone else is already taken.'
+  ];
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('Ninja ID Card'),
+        title: Text('Awesome Quotes'),
         centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
+        backgroundColor: Colors.redAccent,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            ninjaLevel += 1;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[800],
+      body: Column(
+        children: quotes.map((q) {
+          return Text(q);
+        }).toList(),
       ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/tree-736885__340.webp'),
-                radius: 40.0
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[800],
-            ),
-            Text(
-              'Name',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'Hasibul Hasan',
-              style: TextStyle(
-                color: Colors.orange,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'Lavel',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              '$ninjaLevel',
-              style: TextStyle(
-                color: Colors.orange,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'Occupation',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'Sofware Developer',
-              style: TextStyle(
-                color: Colors.orange,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0,),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                SizedBox(width: 10.0,),
-                Text(
-                  'Haisbul_uap@gmail.com',
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
-      )
     );
   }
 }
